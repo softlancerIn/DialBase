112b358341183d5401b2c19d927b0eb3628badbb
Amit Kumar <amitkumarkesharwani98@gmail.com>
2025-12-10 22:58:21 +0530

    Added check

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -2393,63 +2393,65 @@
 
 
     <!-- ======================= Blog Start ============================ -->
-    <section class="space min pt-0">
-        <div class="container">
+    @if ($data['homeBlogs'] ?? [])
+        <section class="space min pt-0">
+            <div class="container">
 
-            <div class="row justify-content-center">
-                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                    <div class="sec_title position-relative text-center mb-4">
-                        <h6 class="theme-cl mb-0">Latest News</h6>
-                        <h2 class="ft-bold">Pickup New Updates</h2>
+                <div class="row justify-content-center">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                        <div class="sec_title position-relative text-center mb-4">
+                            <h6 class="theme-cl mb-0">Latest News</h6>
+                            <h2 class="ft-bold">Pickup New Updates</h2>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="row justify-content-center">
-                @foreach(($data['homeBlogs'] ?? []) as $blog)
-                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
-                        <div class="gup_blg_grid_box">
-                            <div class="gup_blg_grid_thumb">
-                                <a href="{{ route('web_blog_details', $blog->slug) }}">
-                                    <img src="{{ $blog->image ? asset('upload_image/blog/'.$blog->image) : asset('assets/img/b-4.jpg') }}" class="img-fluid"
-                                         alt="{{ $blog->name }}" style="width:100%;aspect-ratio:16/9;object-fit:cover;display:block;">
-                                </a>
-                            </div>
-                            <div class="gup_blg_grid_caption">
-                                <div class="blg_tag"><span>{{ optional($blog->category)->name ?? 'Blog' }}</span></div>
-                                <div class="blg_title">
-                                    <h4><a href="{{ route('web_blog_details', $blog->slug) }}">{{ $blog->name }}</a></h4>
+                <div class="row justify-content-center">
+                    @foreach(($data['homeBlogs'] ?? []) as $blog)
+                        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                            <div class="gup_blg_grid_box">
+                                <div class="gup_blg_grid_thumb">
+                                    <a href="{{ route('web_blog_details', $blog->slug) }}">
+                                        <img src="{{ $blog->image ? asset('upload_image/blog/'.$blog->image) : asset('assets/img/b-4.jpg') }}" class="img-fluid"
+                                            alt="{{ $blog->name }}" style="width:100%;aspect-ratio:16/9;object-fit:cover;display:block;">
+                                    </a>
                                 </div>
-                                <div class="blg_desc">
-                                    <p>{{ \Illuminate\Support\Str::limit(strip_tags($blog->description), 120) }}</p>
-                                </div>
-                            </div>
-                            <div class="crs_grid_foot">
-                                <div class="crs_flex d-flex align-items-center justify-content-between br-top px-3 py-2">
-                                    <div class="crs_fl_first">
-                                        <div class="crs_tutor">
-                                            <div class="crs_tutor_thumb"><a href="javascript:void(0);"><img src="{{ asset('assets/img/team-2.jpg') }}" class="img-fluid circle" width="35" alt="Admin"></a></div>
-                                        </div>
+                                <div class="gup_blg_grid_caption">
+                                    <div class="blg_tag"><span>{{ optional($blog->category)->name ?? 'Blog' }}</span></div>
+                                    <div class="blg_title">
+                                        <h4><a href="{{ route('web_blog_details', $blog->slug) }}">{{ $blog->name }}</a></h4>
                                     </div>
-                                    <div class="crs_fl_last">
-                                        <div class="foot_list_info">
-                                            <ul>
-                                                <li>
-                                                    <div class="elsio_ic"><i class="fa fa-clock text-warning"></i></div>
-                                                    <div class="elsio_tx">{{ optional($blog->created_at)->format('d M Y') }}</div>
-                                                </li>
-                                            </ul>
+                                    <div class="blg_desc">
+                                        <p>{{ \Illuminate\Support\Str::limit(strip_tags($blog->description), 120) }}</p>
+                                    </div>
+                                </div>
+                                <div class="crs_grid_foot">
+                                    <div class="crs_flex d-flex align-items-center justify-content-between br-top px-3 py-2">
+                                        <div class="crs_fl_first">
+                                            <div class="crs_tutor">
+                                                <div class="crs_tutor_thumb"><a href="javascript:void(0);"><img src="{{ asset('assets/img/team-2.jpg') }}" class="img-fluid circle" width="35" alt="Admin"></a></div>
+                                            </div>
+                                        </div>
+                                        <div class="crs_fl_last">
+                                            <div class="foot_list_info">
+                                                <ul>
+                                                    <li>
+                                                        <div class="elsio_ic"><i class="fa fa-clock text-warning"></i></div>
+                                                        <div class="elsio_tx">{{ optional($blog->created_at)->format('d M Y') }}</div>
+                                                    </li>
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
-            </div>
+                    @endforeach
+                </div>
 
-        </div>
-    </section>
+            </div>
+        </section>
+    @endif
     <!-- ======================= Blog Start ============================ -->
 
     <!-- ============================ Pricing Start ==================================== -->
