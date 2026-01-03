666efd9a5071079ae37de4f184898f91b39a6d62
softlancerIn <softlancer.in@gmail.com>
2026-01-03 13:26:53 +0530

    Rename route from 'all-blogs' to 'blogs'

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::controller(WebController::class)->group(function () {
 
     // our product
     Route::get('all-categories', 'all_categories')->name('all_categories');
-    Route::get('all-blogs', 'all_blogs')->name('all_blogs');
+    Route::get('blogs', 'all_blogs')->name('all_blogs');
     Route::get('{slug}.html', 'master_function')->name('products');
 
     // blog
